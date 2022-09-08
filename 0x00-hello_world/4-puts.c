e2f7494c802191f077c805040ff69baca383b25b
harry1994-eng <azubuikeharry209@gmail.com>
2022-09-08 08:07:57 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main -entry point
+ *
+ * return:always 0 (success)
+ */
+int main (void)
+{
+	  put("\"Programming is like building a multilingual puzzle
+	     );
+	  return (0);
+}
