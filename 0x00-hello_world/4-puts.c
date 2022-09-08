5bdbc5d4ef41ee564fe9d8ca2f14432c2723f9b7
harry1994-eng <azubuikeharry209@gmail.com>
2022-09-08 14:00:40 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,7 +6,6 @@
  * return: Always 0 (success)
  */
  int main(void)
- {
-       puts("\" programming is like building a mutilingual puzzle");
-        return (0);
+ {puts("\"programming is like building a mutilingual puzzle");
+return(0);
 } 
