141f4906946370c6490e68e2baeefe48da97e7fb
harry1994-eng <azubuikeharry209@gmail.com>
2022-09-08 11:31:19 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
-#include <stdio.h>
+ #include <stdio.h>
 
-/**
-* main - print the string in the put function 
-*
-* return:Always 0 (success)
-*/
+ /**
+ * main - Entry point 
+ *
+ * return: Always 0 (success)
+ */
  int main(void)
-{
-  puts("/" programming is like building a mutilingual puzzle
+ {
+       puts("\" programming is like building a mutilingual puzzle
  );
- return (0);
+        return (0);
+} 
