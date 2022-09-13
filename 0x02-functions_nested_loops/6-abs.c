31b39ff4560562425afec51c948ce8a61540685d
harry1994-eng <azubuikeharry209@gmail.com>
2022-09-13 10:33:16 -0700

    6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * _abs - a function
- * interger input
- * Return:
+ * _abs - a function that computes the absolute value of an integer
+ *@ab: interger input
+ * Return: absolute valve of ab
  */
 int _abs(int ab)
 {
