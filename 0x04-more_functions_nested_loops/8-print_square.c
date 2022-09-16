692c6debec49dc05c0f3051d510004553795a16a
harry1994-eng <azubuikeharry209@gmail.com>
2022-09-15 22:14:47 -0700

    8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -18,7 +18,7 @@ void print_square(int size)
 		for (co = 1; co <= size; co++)
 		{
 			_putchar('#');
-			for (ro = 2; co <= size; ro++)
+			for (ro = 2; ro <= size; ro++)
 			{
 				_putchar('#');
 			}
