4cd72337dd313f381dd30ffc9409d5a07fd29e96
harry1994-eng <azubuikeharry209@gmail.com>
2022-09-19 13:00:02 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -13,11 +13,9 @@ void print_rev(char *s)
 	{
 		fcounter++;
 	}
-	for (n = (fcounter - 1); n >= 0; n--)
-	
+	for (n = (fcounter - 1); n >= 0; n--)	
 	{
 		_putchar(s[n]);
 	}
-	
 	_putchar('\n');
 }
