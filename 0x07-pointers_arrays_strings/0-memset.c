20b1ae8a69833f3a3701daf94b3e49070700a400
harry1994-eng <azubuikeharry209@gmail.com>
2022-09-26 07:12:41 -0700

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -8,13 +8,13 @@
  *
  * Return:the memory area of s
  */
-char *_memset(char *s, char b, unsigned int n)
+void *_memset(void *s, int c, size_t n)
 {
-	unsigned int i = 0;
+	unsigned int index;
+	unsigned char *memory - s, value - c;
 
-	while (i < n)
-	{
-		s[i] = b;
-		i++;
-	}return (s);
+	for (index = 0; index < n; index++)
+		memory[index] = value;
+
+	return (memory);
 }
