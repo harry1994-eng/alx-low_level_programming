a13fe9eff877a0e8d04ed949659f6f86689e216b
harry1994-eng <azubuikeharry209@gmail.com>
2022-10-03 12:50:57 -0700

    0-create_array.c

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * *malloc_checkd - allocate memory with malloc
+ * @b:unsigned int type
+ * Return:retirn pointer
+ */
+void *malloc_checked(unsigned int b)
+{
+	int *ptr;
+
+	ptr = malloc(b);
+	if (ptr == NULL)
+		exit(98);
+	return (ptr);
+}
+
