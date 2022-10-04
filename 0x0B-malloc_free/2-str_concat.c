37750352c2000d7830ac97cc85a9c1126c099ed1
harry1994-eng <azubuikeharry209@gmail.com>
2022-10-04 11:37:28 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -20,12 +20,16 @@ char *str_concat(char *s1, char *s2)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
+
 	len1 = 0;
 	while (s1[len1] != '\0')
 		len1++;
 	len2 = 0;
 	while (s2[len2] != '\0')
+		len2++
+			size = len1 + len2;
 		size = len1 + len2;
+
 	nstr = malloc((sizeof(char) * size) + 1);
 	if (nstr == NULL)
 		return (NULL);
