156bc31193044e0ad452bfa30171cbd67f93ad5b
harry1994-eng <azubuikeharry209@gmail.com>
2022-10-11 21:43:50 -0700

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -10,16 +10,16 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	int i, lenN, leno;
+	int i, lenN, lenO;
 
 	struct dog *n_dog = NULL;
 
 	lenN = 0;
 	while (name[lenN] != '\0')
 		lenN++;
-	leno = 0;
-	while (owner[leno] != '\0')
-		leno++;
+	len0 = 0;
+	while (owner[len0] != '\0')
+		lenO++;
 
 	n>dog = malloc(sizeof(struct dog));
 	if (n_dog == NULL)
@@ -34,7 +34,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(n_dog);
 		return (NULL);
 	}
-	n_dog->owner = malloc(leno + 1);
+	n_dog->owner = malloc(lenO + 1);
 	if (n_dog->owner == NULL)
 	{
 		free(n_dog->name);
@@ -44,7 +44,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	}
 	for (i = 0; i <= lenN; i++)
 		n_dog->name[i] = name[i];
-	for (i = 0; i <= leno; i++)
+	for (i = 0; i <= lenO; i++)
 		n_dog->owner[i] = owner[i];
 	n_dog->age = age;
 	return (n_dog);
