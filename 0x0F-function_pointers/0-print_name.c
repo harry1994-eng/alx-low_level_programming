8dbcf99a66b7704732220794766ddb790f1e1e9a
harry1994-eng <azubuikeharry209@gmail.com>
2022-10-12 19:59:28 -0700

    0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+/**
+ * prinit_name - prinits a name
+ * @name pointer to name
+ * @f: function pointer
+ *
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name && f)
+		f(name);
+}
